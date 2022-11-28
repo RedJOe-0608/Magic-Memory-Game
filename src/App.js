e045877af823bc57308079772939343b65112729
Jyothi <joeswaroopmakena@gmail.com>
2022-11-28 19:41:07 +0530

    Card Images array created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 function App() {
-  return <div className="App"></div>;
+  const cardImages = [
+    {"src": "/img/helmet-1.png"},
+    {"src": "/img/potion-1.png"},
+    {"src": "/img/ring-1.png"},
+    {"src": "/img/scroll-1.png"},
+    {"src": "/img/shield-1.png"},
+    {"src": "/img/sword-1.png"},
+  ]
+  return <div className="flex flex-col items-center justify-center">
+    <h1 className="text-5xl text-white mt-[4rem]">Magic Memory!</h1>
+    <button className="mt-[3rem] rounded-md p-3 bg-white">New Game</button>
+    <main>
+
+    </main>
+  </div>;
 }
 
 export default App;

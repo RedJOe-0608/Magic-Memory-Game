d631c6b89a1a72e39dcbb5e579064980929a2bd6
Jyothi <joeswaroopmakena@gmail.com>
2022-11-28 20:38:58 +0530

    Game working in console log

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MagicCard from "./components/MagicCard";
 
 function App() {
 
   const [cards, setCards] = useState([])
-
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
   const cardImages = [
     {"src": "/img/helmet-1.png"},
     {"src": "/img/potion-1.png"},
@@ -16,15 +17,38 @@ function App() {
 
   const shuffleCards = () => {
     const shuffledCards = [...cardImages,...cardImages]
-    .sort((a,b) => Math.random() - 0.5)
+    .sort(() => Math.random() - 0.5)
     .map((card) => ({...card, id: Math.ceil(Math.random() * 1000)}))
     setCards(shuffledCards)
   }
 
+  const handleChoice = (card) =>{
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
 
+  useEffect(() => {
+  if(choiceTwo !=null)
+  {
 
+      if(choiceOne.src === choiceTwo.src)
+      {
+        console.log("The selected images are equal");
+        reset()
+      }
+      else
+      {
+        console.log("The selected images are not equal");
+        reset()
+      }
+  }
+},[choiceOne, choiceTwo])
 
-  return <div className="flex flex-col mx-auto max-w-[860px] items-center justify-center">
+  const reset = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+  }
+
+ return <div className="flex flex-col mx-auto max-w-[860px] items-center justify-center">
     <h1 className="text-5xl text-white mt-[4rem]">Magic Memory!</h1>
     <button 
       onClick={shuffleCards}
@@ -32,7 +56,10 @@ function App() {
     <main className="grid grid-cols-4 mt-[3rem] gap-4">
       {cards.map((card) => {
         return (
-          <MagicCard  card={card}/>
+          <MagicCard  
+          handleChoice={handleChoice}
+            card={card}
+          />
         )
       })}
     </main>
--- src/components/MagicCard.jsx
+++ src/components/MagicCard.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const MagicCard = ({card}) => {
+const MagicCard = ({card, handleChoice}) => {
+    const handleClick = () => {
+
+        handleChoice(card)
+    }
   return (
     <div>
-        <img src={card.src} className='w-[8rem] h-[8rem]' />
+        <img src={card.src}
+            onClick={handleClick}
+            className='w-[8rem] h-[8rem]' 
+        />
+        <img src='/img/cover.png' className='w-[8rem] h-[8rem]' />
     </div>
   )
 }

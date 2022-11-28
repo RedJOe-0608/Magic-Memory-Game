610fdfe947e348c2dde7621d47b2559ee5f5bd86
Jyothi <joeswaroopmakena@gmail.com>
2022-11-28 19:51:32 +0530

    Shuffled Cards state created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 function App() {
+
+  const [cards, setCards] = useState([])
+
   const cardImages = [
     {"src": "/img/helmet-1.png"},
     {"src": "/img/potion-1.png"},
@@ -7,9 +12,22 @@ function App() {
     {"src": "/img/shield-1.png"},
     {"src": "/img/sword-1.png"},
   ]
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages,...cardImages]
+    .sort((a,b) => Math.random() - 0.5)
+    .map((card) => ({...card, id: Math.ceil(Math.random() * 1000)}))
+  }
+
+  setCards(shuffleCards)
+
+
+
   return <div className="flex flex-col items-center justify-center">
     <h1 className="text-5xl text-white mt-[4rem]">Magic Memory!</h1>
-    <button className="mt-[3rem] rounded-md p-3 bg-white">New Game</button>
+    <button 
+      onClick={shuffleCards}
+      className="mt-[3rem] rounded-md p-3 bg-white">New Game</button>
     <main>
 
     </main>
